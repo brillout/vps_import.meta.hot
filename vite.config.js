298f05777a029cc6f43ec9e70b6f3c7d316850e2
Romuald Brillout <git@brillout.com>
2023-02-10 18:04:58 +0100

    debug

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
+import react from "@vitejs/plugin-react";
+import ssr from "vite-plugin-ssr/plugin";
 
 export default {
-  plugins: [react(), ssr()]
-}
+  plugins: [
+    react(),
+    ssr(),
+    {
+      plugin: "tmp-plugin",
+      configResolved(config) {
+        console.log("config.isProduction", config.isProduction);
+      },
+    },
+  ],
+};
